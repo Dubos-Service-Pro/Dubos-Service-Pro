c5d2f448fbcd43488030be415feea82eeaac5114
Dubos-Service-Pro <leo2lyon6906@gmail.com>
2026-08-19 16:39:03 +0200

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const formulaire = document.querySelector("form");
 
-formulaire.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    alert(" Merci ! Votre demande a bien été prise en compte. ✅");
+formulaire.addEventListener("submit", function() {
+    alert("Merci ! Votre demande va être envoyée. ✅");
 });
+
+console.log("Dubos Service Pro - site chargé");
